de11720f252e5e10c35460a1830e30098a9bed35
MarcCanellasG <marccg8@gmail.com>
2023-10-26 15:43:37 +0200

    cambios

--- 04_Non_linear_SVM/main.py
+++ 04_Non_linear_SVM/main.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score
 from sklearn.svm import SVC
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.spatial import distance_matrix
 from sklearn.preprocessing import MinMaxScaler
 
@@ -20,3 +21,23 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 scaler = MinMaxScaler() #StandardScaler()
 X_transformed = scaler.fit_transform(X_train)
 X_test_transformed = scaler.transform(X_test)
+
+
+def kernel_lineal(x1, x2):
+    return x1.dot(x2.T)
+
+def gaussian_kernel(x1, x2, gamma = 10):
+    inv = gamma
+    dist = distance_matrix(x1,x2)
+    return np.exp(np.dot(-inv,dist**2))
+
+def kernel_poly(x1, x2, degree=3):
+    return (x1.dot(x2.T))**degree
+
+
+svm = SVC(C=1.0, kernel=kernel_poly, random_state=33)
+svm.fit(X_transformed, y_train)
+y_meu = svm.predict(X_test_transformed)
+
+accuracy1 = precision_score(y_test,y_meu)
+print(accuracy1)
